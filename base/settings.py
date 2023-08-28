ba7dce7525be4c1a857827a8bccc01571272b615
Andressa-Anthero7 <andressa.anthero7@gmail.com>
2023-08-28 19:38:02 -0300

    revisao deploy 28-8-2023 v-3

--- base/settings.py
+++ base/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-45)3+-23ediw3s$q(*l2)2+^$o8j@q&!utxurs=&sjj(bv(yv4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['54.196.120.245', '127.0.0.1']
+ALLOWED_HOSTS = ['54.196.120.245', '44.210.203.94']
 
 # Application definition
 
